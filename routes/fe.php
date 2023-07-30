0ea441200d51fbd8f15c5560efb28a322705f689
MinhHoang2104 <133194170+MinhHoang2104@users.noreply.github.com>
2023-07-30 10:18:45 +0700

    update layout, add HomeController

--- routes/fe.php
+++ routes/fe.php
@@ -1,2 +1,13 @@
 <?php
+
+use App\Http\Controllers\Admin\CommentController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Models\Film;
 use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('/')->group(function () {
+    Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('user.index');
+
+});
+
