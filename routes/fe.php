9bad04ef44827ad62b41b26eb70d9f2b8b5a03d6
thienhuong661 <133196415+thienhuong661@users.noreply.github.com>
2023-07-30 21:32:09 +0700

    update route fe

--- routes/fe.php
+++ routes/fe.php
@@ -8,5 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/')->group(function () {
     Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('user.index');
 
+    Route::get('/movie-details/{id}', [\App\Http\Controllers\HomeController::class, 'movieDetails'])->name('user.movie-details');
+
+    Route::get('/play-movie/{id}', [\App\Http\Controllers\HomeController::class, 'playMovie'])->name("user.play-movie");
+
 });
 
