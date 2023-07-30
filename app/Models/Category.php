5b41ac37428cb55d1b203d7d80abc6cb7c419352
phudcc <phudcc@example.com>
2023-07-30 09:19:08 +0700

    update model category, country, film and add relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable=['name'];
+    public function films(){
+        return $this->belongsToMany(Film::class, 'film_categories', 'category_id', 'film_id');
+    }
 }
--- app/Models/Film.php
+++ app/Models/Film.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Film extends Model
 {
     use HasFactory;
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'path',
+        'year',
+        'country_id',
+        'director_id',
+        'duration',
+        'is_hot',
+        'is_oscar'
+    ];
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'film_categories', 'film_id', 'category_id');
+    }
+
+    public function countries()
+    {
+        return $this->belongsTo(Country::class, 'country_id');
+    }
 }
