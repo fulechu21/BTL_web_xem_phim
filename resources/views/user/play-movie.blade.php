aa5e4dfed72052f868cba205f81076fdee160617
thienhuong661 <133196415+thienhuong661@users.noreply.github.com>
2023-07-30 21:30:18 +0700

    fix layout play-movice

--- resources/views/user/play-movie.blade.php
+++ resources/views/user/play-movie.blade.php
@@ -6,8 +6,11 @@
                 <div class="panel-heading">
                     <div class="row">
                         <div class="col-xs-6">
-                            <div class="yoast_breadcrumb hidden-xs"><span><span><a href="{{route('user.index')}}">Trang chủ</a> » <span
-                                            class="breadcrumb_last" aria-current="page">Phim mới</span></span></span></div>
+                            <div class="yoast_breadcrumb hidden-xs"><span><span><a href="">Phim hay</a> » <span><a
+                                                href="{{route('user.newFilms')}}">Trung Quốc</a> » <span
+                                                class="breadcrumb_last"
+                                                aria-current="page">{{$filmPlay->name}}</span></span></span></span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -16,40 +19,141 @@
                 </div>
             </div>
             <main id="main-contents" class="col-xs-12 col-sm-12 col-md-8">
-                <section>
-                    <div class="section-bar clearfix">
-                        <h1 class="section-title"><span>Phim 2020</span></h1>
-                    </div>
-                    <div class="halim_box">
+                <section id="content" class="test">
+                    <div class="clearfix wrap-content">
 
-                        @foreach($films as $film)
-                            <article class="col-md-3 col-sm-3 col-xs-6 thumb grid-item post-27021">
-                                <div class="halim-item">
-                                    <a class="halim-thumb" href="{{route('user.movie-details',['id'=>$film->id])}}"
-                                       title="{{$film->name}}">
-                                        <figure><img class="lazy img-responsive"
-                                                     src="{{ asset('storage/image/'.$film->image) }}"
-                                                     alt="{{$film->name}}" title="{{$film->name}}"></figure>
-                                        <span class="status">5/5</span><span class="episode"><i class="fa fa-play"
-                                                                                                aria-hidden="true"></i>Vietsub</span>
-                                        <div class="icon_overlay"></div>
-                                        <div class="halim-post-title-box">
-                                            <div class="halim-post-title ">
-                                                <p class="entry-title">{{$film->name}}</p>
+                        <iframe width="100%" height="500" src="https://www.youtube.com/embed/jwiYWq43FMs"
+                                title="YouTube video player" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
 
-                                            </div>
-                                        </div>
+                        <div class="button-watch">
+                            <ul class="halim-social-plugin col-xs-4 hidden-xs">
+                                <li class="fb-like" data-href="" data-layout="button_count" data-action="like"
+                                    data-size="small" data-show-faces="true" data-share="true"></li>
+                            </ul>
+                        </div>
+                        <div class="collapse" id="moretool">
+                            <ul class="nav nav-pills x-nav-justified">
+                                <li class="fb-like" data-href="" data-layout="button_count" data-action="like"
+                                    data-size="small" data-show-faces="true" data-share="true"></li>
+                                <div class="fb-save" data-uri="" data-size="small"></div>
+                            </ul>
+                        </div>
+
+                        <div class="clearfix"></div>
+                        <div class="clearfix"></div>
+
+                        <div class="entry-content htmlwrap clearfix collapse" id="expand-post-content">
+                            <article id="post-37976" class="item-content post-37976"></article>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="text-center">
+                            <div id="halim-ajax-list-server"></div>
+                        </div>
+                        <div id="halim-list-server">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li role="presentation" class="active server-1">
+                                    <a href="#server-0" aria-controls="server-0" role="tab"
+                                       data-toggle="tab"><i class="hl-server"></i> Vietsub
                                     </a>
+                                </li>
+                            </ul>
+                            <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane active server-1" id="server-0">
                                 </div>
-                            </article>
-                        @endforeach
-
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="htmlwrap clearfix">
+                            <div id="lightout"></div>
+                        </div>
                     </div>
-                    <div class="clearfix"></div>
-                    <div class="text-center">
-                        <ul class='page-numbers'>
-                            {{$films->links()}}
-                        </ul>
+                </section>
+
+
+                <!-- start-comment -->
+                <div class="wrapper" style="background-color: white;">
+                    <div class="" style="margin: 0 30px">
+                        <form style="margin-bottom: 10px" method="POST" action="{{ route('comments.store') }}">
+                            @csrf
+                            <textarea class="nd-comments" placeholder="Viết bình luận ..." name="content"></textarea>
+
+                            <input type="hidden" name="film_id" value="{{ $film->id }}">
+                            <button class="btn-submit" type="submit">Submit</button>
+                        </form>
+                        <div id="commentsContainer" style="max-height: 500px;overflow-y: auto">
+                            @foreach($comments as $index => $comment)
+                                <div class="comment"
+                                     style="display: {{ $index < 5 ? 'flex' : 'none' }};margin-bottom: 15px; gap: 10px">
+                                    <div><img src="{{ asset('user/img/user.png') }}" alt="" width="30"></div>
+                                    <div>
+                                        <h5 class="text-primary"
+                                            style="font-weight: 600">{{ $comment->users->name }}</h5>
+                                        <p>{{ $comment->content }}</p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+
+                        @if ($comments->count() > 5)
+                            <button id="showMoreBtn" class="btn-show-more" style="margin-bottom: 20px"
+                                    onclick="showMoreComments()">Show more
+                            </button>
+                        @endif
+                    </div>
+                    <script !src="">
+                        function showMoreComments() {
+                            // Ẩn nút "Show more"
+                            document.getElementById('showMoreBtn').style.display = 'none';
+
+                            // Hiển thị tất cả các comment
+                            var comments = document.getElementsByClassName('comment');
+                            for (var i = 0; i < comments.length; i++) {
+                                comments[i].style.display = 'flex';
+                            }
+                        }
+                    </script>
+
+                </div>                <!-- end-comment -->
+
+                <section class="related-movies">
+                    <div id="halim_related_movies-2xx" class="wrap-slider">
+                        <div class="section-bar clearfix">
+                            <h3 class="section-title"><span>CÓ THỂ BẠN MUỐN XEM</span></h3>
+                        </div>
+                        <div id="halim_related_movies-2" class="owl-carousel owl-theme related-film">
+
+
+                        </div>
+                        <div class="image-slider filtering">
+                            @foreach($filmsHot as $film)
+                                <div class="image-item">
+                                    <div class="image">
+                                        <a href={{route('user.movie-details',['id'=>$film->id])}}> <img
+                                                src="{{ asset('storage/image/'.$film->image) }}" alt="{{$film->name}}">
+                                        </a>
+                                        <p class="entry-title">{{$film->name}}</p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                        <script>
+                            jQuery(document).ready(function ($) {
+                                var owl = $('#halim_related_movies-2');
+                                owl.owlCarousel({
+                                    loop: true,
+                                    margin: 4,
+                                    autoplay: true,
+                                    autoplayTimeout: 4000,
+                                    autoplayHoverPause: true,
+                                    nav: true,
+                                    navText: ['<i class="hl-down-open rotate-left"></i>', '<i class="hl-down-open rotate-right"></i>'],
+                                    responsiveClass: true,
+                                    responsive: {0: {items: 2}, 480: {items: 3}, 600: {items: 4}, 1000: {items: 4}}
+                                })
+                            });
+                        </script>
                     </div>
                 </section>
             </main>
@@ -58,35 +162,38 @@
                     <div class="section-bar clearfix">
                         <div class="section-title">
                             <span>Top Views</span>
-                            {{--                        <ul class="halim-popular-tab" role="tablist">--}}
-                            {{--                           <li role="presentation" class="active">--}}
-                            {{--                              <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10" data-type="today">Day</a>--}}
-                            {{--                           </li>--}}
-                            {{--                           <li role="presentation">--}}
-                            {{--                              <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10" data-type="week">Week</a>--}}
-                            {{--                           </li>--}}
-                            {{--                           <li role="presentation">--}}
-                            {{--                              <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10" data-type="month">Month</a>--}}
-                            {{--                           </li>--}}
-                            {{--                           <li role="presentation">--}}
-                            {{--                              <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10" data-type="all">All</a>--}}
-                            {{--                           </li>--}}
-                            {{--                        </ul>--}}
+                            {{--                            <ul class="halim-popular-tab" role="tablist">--}}
+                            {{--                                <li role="presentation" class="active">--}}
+                            {{--                                    <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10"--}}
+                            {{--                                       data-type="today">Day</a>--}}
+                            {{--                                </li>--}}
+                            {{--                                <li role="presentation">--}}
+                            {{--                                    <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10"--}}
+                            {{--                                       data-type="week">Week</a>--}}
+                            {{--                                </li>--}}
+                            {{--                                <li role="presentation">--}}
+                            {{--                                    <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10"--}}
+                            {{--                                       data-type="month">Month</a>--}}
+                            {{--                                </li>--}}
+                            {{--                                <li role="presentation">--}}
+                            {{--                                    <a class="ajax-tab" role="tab" data-toggle="tab" data-showpost="10" data-type="all">All</a>--}}
+                            {{--                                </li>--}}
+                            {{--                            </ul>--}}
                         </div>
                     </div>
                     <section class="tab-content">
                         <div role="tabpanel" class="tab-pane active halim-ajax-popular-post">
                             <div class="halim-ajax-popular-post-loading hidden"></div>
                             <div id="halim-ajax-popular-post" class="popular-post">
-
                                 @foreach($filmsViews as $film)
                                     <div class="item post-37176">
-                                        <a href="{{route('user.play-movie',['id'=>$film->id])}}"
+                                        <a href="{{route('user.movie-details',['id'=>$film->id])}}"
                                            title="{{$film->name}}">
                                             <div class="item-link">
-                                                <img src="{{ asset('storage/image/'.$film->image) }}"
-                                                     class="lazy post-thumb" alt="{{$film->name}}"
-                                                     title="{{$film->name}}"/>
+                                                <img
+                                                    src="{{asset('storage/image/'.$film->image)}}"
+                                                    class="lazy post-thumb" alt="{{$film->name}}"
+                                                    title="{{$film->name}}"/>
                                             </div>
                                             <p class="title">{{$film->name}}</p>
                                         </a>
@@ -99,6 +206,7 @@
                                         </div>
                                     </div>
                                 @endforeach
+
 
                             </div>
                         </div>
