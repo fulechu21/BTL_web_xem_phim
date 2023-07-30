3eab113945553c170bd936bf236815787e02470f
Vupham-SD2 <135797786+Vupham-SD2@users.noreply.github.com>
2023-07-30 19:00:03 +0700

    fix layout change password, register, login confirm-password, reset-password, verify-email, forgot-password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img src={{asset('user/img/logo_auth.png')}}  alt="Logo" class="w-30 h-20 fill-current text-gray-500">
             </a>
         </x-slot>
 
