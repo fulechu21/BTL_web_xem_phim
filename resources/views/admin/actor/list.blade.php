e3db056403e1248e364de3e13c052e9917ecb9c4
MinhHoang2104 <133194170+MinhHoang2104@users.noreply.github.com>
2023-08-02 23:19:22 +0700

    add crud actor, category country, director

---
+++ resources/views/admin/actor/list.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.layout')
+@section('main-content')
+    <div class="card">
+        <div class="card-header">
+            <a href="{{route('admin.actor.create')}}" class="btn btn-info">Thêm mới</a>
+        </div>
+
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th style="width: 10px">#</th>
+                    <th>Name</th>
+                    <th style="width: 150px">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($list as $item)
+                    <tr>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->name}}</td>
+                        <td>
+                            <a href="{{route('admin.actor.edit',['id'=>$item->id])}}" class="btn btn-warning">Sửa</a>
+                            <a onclick="return confirm('Bạn có chắc muốn xóa ?')"
+                               href="{{route('admin.actor.delete',['id'=>$item->id])}}"
+                               class="btn btn-danger">Xoá</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer clearfix">
+            {{$list->links()}}
+        </div>
+    </div>
+@endsection
+
+@section('breadcrumb')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+        <li class="breadcrumb-item">
+            actor
+        </li>
+
+    </ol>
+@endsection
+
+@section('title')
+    List Actor
+@endsection
