13b74c6eaae8a7558ea7c3e192a2ff8198d8264e
thienhuong661 <133196415+thienhuong661@users.noreply.github.com>
2023-08-02 23:39:19 +0700

    update route fr

--- routes/fe.php
+++ routes/fe.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CommentController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Models\Film;
 use Illuminate\Support\Facades\Route;
@@ -9,10 +10,21 @@ Route::prefix('/')->group(function () {
     Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('user.index');
 
     Route::get('/movie-details/{id}', [\App\Http\Controllers\HomeController::class, 'movieDetails'])->name('user.movie-details');
+    Route::get('/new-films', [\App\Http\Controllers\HomeController::class, 'filmsNew'])->name('user.newFilms');
+    Route::get('/oscar-films', [\App\Http\Controllers\HomeController::class, 'filmsOscar'])->name('user.oscarFilms');
+    Route::get('/category/{option}', [\App\Http\Controllers\HomeController::class, 'filmsCategory'])->name('user.filmsCategory');
+    Route::get('/country/{option}', [\App\Http\Controllers\HomeController::class, 'filmsCountry'])->name('user.filmsCountry');
 
     Route::get('/play-movie/{id}', [\App\Http\Controllers\HomeController::class, 'playMovie'])->name("user.play-movie");
 
+    Route::post('/comments', [CommentController::class, "store"])->name('comments.store');
+    Route::get("/show-comments", [CommentController::class, "showComments"])->name('showComments');
+    Route::get('/load-more-commentes', [CommentController::class, "loadMoreComments"])->name('user.loadMoreComments');
+
     Route::get("show-favorite-film/{id}", [\App\Http\Controllers\Admin\FilmController::class, 'showFilmFavorite'])->name("user.showFilmFavorite");
     Route::post("/add_favorite", [\App\Http\Controllers\Admin\FilmController::class, "addFilmFavorite"])->name("addFavorite");
+
+
 });
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
